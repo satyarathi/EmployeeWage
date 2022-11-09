8caef49354c0591b877323aa162d35895169e202
satyarathi <jitendrasatyarathi@gmail.com>
2022-11-09 13:16:55 +0530

    seventh

--- EmployeeWageComp.java
+++ EmployeeWageComp.java
@@ -7,29 +7,33 @@ public class EmployeeWageComp {
 		final int present = 2;
 		int wage_per_hr = 20;
 		int working_hr = 0;
-		int working_days = 20;
 		int total_wage = 0;
+		int max_working_days = 20;
+		int max_working_hrs = 100;
 
-		for (int day = 1; day <= working_days;day++)
+
+		System.out.printf("%4s   %4s    %4s   %4s\n", "Day","workinghrs","wage","total working hrs");
+
+		for (int day = 1, totalworkinghrs = 0; day <= max_working_days && totalworkinghrs < max_working_hrs; day++, totalworkinghrs += working_hr)
 		{
 			int empCheck = (int)(Math.random() * 10) % 3;
 			switch (empCheck)
 			{
 			case present:
-				System.out.println("Employee is present full time");
 				working_hr = 8;
 				break;
 			case part_time:
-				System.out.println("Employee is working Part time");
 				working_hr = 6;
 				break;
 
 			default:
-				System.out.println("Employee is Absent" );
+				working_hr = 0;
+				break;
 			}
 			int wage = working_hr*wage_per_hr;
-			System.out.println("days :" +day+ ", wage : " +wage);
+
 			total_wage = total_wage +wage;
+			System.out.printf("%4s   %4s    %7s   %4s\n", day,working_hr,wage,totalworkinghrs+working_hr);
 		}
 		System.out.println("Total wage for a month :" +total_wage);
 	}
