912bae933f53b1568c6d7d2a25e7bfc9b68c98aa
satyarathi <jitendrasatyarathi@gmail.com>
2022-11-09 11:35:34 +0530

    third

--- EmployeeWageComp.java
+++ EmployeeWageComp.java
@@ -2,17 +2,23 @@ package com.dayeight.one;
 
 public class EmployeeWageComp {
 	public static void main(String[] args) {
-		
-	System.out.println("Welcome to the Employee Wage Computation Programm");
-//attandance
+
+
 		int present = 1;
-				
+		int wage_per_hr = 20;
+		int working_hr = 0;
+
 		int empCheck = (int)(Math.random() * 10) % 2;
-		if(empCheck == present)
+		if(empCheck == present) {
 			System.out.println("Employee is present");
-		else
+			working_hr = 8;
+		}
+		else {
 			System.out.println("Employee is Absent" );
 
+		}
+		int wage = working_hr*wage_per_hr;
+		System.out.println("Daily wage of Employee is " +wage);
 	}
 }
 
