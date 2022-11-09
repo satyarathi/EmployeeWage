aefa5c3a675beee359505df9ea914c6d3efd643e
satyarathi <jitendrasatyarathi@gmail.com>
2022-11-09 10:05:52 +0530

    first

---
+++ EmployeeWageComp.java
@@ -0,0 +1,9 @@
+package com.dayeight.one;
+
+public class EmployeeWageComp {
+	public static void main(String[] args) {
+		
+	System.out.println("Welcome to the Employee Wage Computation Programm");
+	}
+}
+
