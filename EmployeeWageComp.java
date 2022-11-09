4f9ae1dd91ed438e68b3c9d3c3b1e6bd43295d99
satyarathi <jitendrasatyarathi@gmail.com>
2022-11-09 12:11:14 +0530

    fifth

--- EmployeeWageComp.java
+++ EmployeeWageComp.java
@@ -3,23 +3,25 @@ package com.dayeight.one;
 public class EmployeeWageComp {
 	public static void main(String[] args) {
 
-		int part_time =1;
-		int present = 2;
+		final int part_time =1;
+		final int present = 2;
 		int wage_per_hr = 20;
 		int working_hr = 0;
 
 		int empCheck = (int)(Math.random() * 10) % 3;
-		if(empCheck == present) {
-			System.out.println("Employee is present");
+		switch (empCheck)
+		{
+		case present:
+			System.out.println("Employee is present full time");
 			working_hr = 8;
-		}
-		else if (empCheck == part_time) {
-			System.out.println("Employee is working Parttime");
+		break;
+		case part_time:
+			System.out.println("Employee is working Part time");
 			working_hr = 6;
-		}
-		else {
+		break;
+		
+		default:
 			System.out.println("Employee is Absent" );
-
 		}
 		int wage = working_hr*wage_per_hr;
 		System.out.println("Daily wage of Employee is " +wage);
